4213623eae7eeeeafc9eae4e96ffcd3628445557
Joffre Gavidia <joffregh3002@gmail.com>
2025-04-30 13:18:05 -0500

    Actualizacion de Blog

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from "astro:content";
+
+const blogContenidoCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    resume: z.string(),
+  }),
+});
+
+export const collections = {
+  blog_contenido: blogContenidoCollection,
+};
